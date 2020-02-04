02df0847ab6a685645d66175567fa0d015376cc7
eisendaniel <danieleisen99@gmail.com>
2020-02-05 11:34:39 +1300

    Milestone v1: Fully implemented algorithm, added key input and color gradient shading

--- main.cpp
+++ main.cpp
@@ -1,21 +1,58 @@
 #include <SFML/Graphics.hpp>
+#include <cstdio>
+#include<cmath>
 
-int main(int argc, char **argv)
+double SIZE = 900.0;
+int MAX_N = 11;
+int n_verts = 4;
+
+int increment(int &n)
 {
-	int width = 300, height = 300;
+	n = n + 1 <= MAX_N ? n + 1 : MAX_N;
+	n_verts = 4 * pow(4, n - 1);
+	return n;
+}
 
-	// RENDER DEMO : DISPLAYS LOGO
-	//--------------------------------------
-	sf::Texture texture;
-	sf::Sprite logo;
-	texture.loadFromFile("../res/sfml.png");
-	logo.setTexture(texture);
-	width = texture.getSize().x;
-	height = texture.getSize().y;
-	//--------------------------------------
+int decrement(int &n)
+{
+	n = n - 1 >= 1 ? n - 1 : 1;
+	n_verts = 4 * pow(4, n - 1);
+	return n;
+}
 
-	sf::RenderWindow window(sf::VideoMode(width, height), "Hilbert Curve");
+void
+HilbertCurve(int n, sf::VertexArray &curve, double x0 = 0.0, double y0 = 0.0, double xi = 0.0, double xj = 1.0, double yi = 1.0, double yj = 0.0)
+{
+	if (n <= 0) {
+		double X = x0 + (xi + yi) / 2.0;
+		double Y = y0 + (xj + yj) / 2.0;
+		// Output the coordinates of the curve
+		sf::Color color(
+			255 - 128 * curve.getVertexCount() / n_verts,
+			128 + 128 * curve.getVertexCount() / n_verts,
+			128 * curve.getVertexCount() / n_verts);
+
+		curve.append(sf::Vertex(sf::Vector2f(SIZE * X, SIZE * Y), color));
+		return;
+	}
+	HilbertCurve(n - 1, curve, x0, y0, yi / 2.0, yj / 2.0, xi / 2.0, xj / 2.0);
+	HilbertCurve(n - 1, curve, x0 + xi / 2.0, y0 + xj / 2.0, xi / 2.0, xj / 2.0, yi / 2.0, yj / 2.0);
+	HilbertCurve(
+		n - 1, curve,
+		x0 + xi / 2.0 + yi / 2.0, y0 + xj / 2.0 + yj / 2.0, xi / 2.0, xj / 2.0, yi / 2.0, yj / 2.0);
+	HilbertCurve(n - 1, curve, x0 + xi / 2.0 + yi, y0 + xj / 2.0 + yj, -yi / 2.0, -yj / 2.0, -xi / 2.0, -xj / 2.0);
+
+}
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(SIZE, SIZE), "Hilbert Curve", sf::Style::Default);
 	window.setFramerateLimit(30);
+
+	int n = 1;
+	sf::VertexArray curve(sf::LineStrip);
+
+	HilbertCurve(n, curve);
+//	printf("Actual: %d\tCalculated: %d\n", curve.getVertexCount(), n_verts);
 
 	while (window.isOpen()) {
 		sf::Event event = {};
@@ -23,10 +60,21 @@ int main(int argc, char **argv)
 			if (event.type == sf::Event::Closed) {
 				window.close();
 				break;
+
+			} else if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Up) {
+					curve.clear();
+					HilbertCurve(increment(n), curve);
+				} else if (event.key.code == sf::Keyboard::Down) {
+					curve.clear();
+					HilbertCurve(decrement(n), curve);
+				}
+//				printf("Actual: %d\tCalculated: %d\n", curve.getVertexCount(), n_verts);
+				break;
 			}
 		}
 		window.clear();
-		window.draw(logo);
+		window.draw(curve);
 		window.display();
 	}
 	return 0;
