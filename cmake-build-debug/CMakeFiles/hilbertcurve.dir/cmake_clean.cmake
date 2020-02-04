4d19cc951310b27d754192274c232822d98a82de
eisendaniel <danieleisen99@gmail.com>
2020-02-04 14:48:28 +1300

    init

---
+++ cmake-build-debug/CMakeFiles/hilbertcurve.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hilbertcurve.dir/main.cpp.obj"
+  "hilbertcurve.exe"
+  "hilbertcurve.exe.manifest"
+  "hilbertcurve.pdb"
+  "libhilbertcurve.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hilbertcurve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
