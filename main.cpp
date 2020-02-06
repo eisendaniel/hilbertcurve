972b1efd987e46e4ecff69a5521f54c0dbfb1e66
eisendaniel <danieleisen99@gmail.com>
2020-02-07 11:57:40 +1300

    Fixed all warnings

--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
 #include <SFML/Graphics.hpp>
-#include <cstdio>
 #include<cmath>
 
-double SIZE = 900.0;
-int MAX_N = 11;
-int n_verts = 4;
-bool updated = true;
-bool held = false;
-float zoom = 1.0;
+#define SIZE 900.0
+#define MAX_N 11
+int vertices = 4;
 
 int increment(int &n)
 {
 	n = n + 1 <= MAX_N ? n + 1 : MAX_N;
-	n_verts = 4 * pow(4, n - 1);
+	vertices = int(4 * pow(4, n - 1));
 	return n;
 }
 
 int decrement(int &n)
 {
 	n = n - 1 >= 1 ? n - 1 : 1;
-	n_verts = 4 * pow(4, n - 1);
+	vertices = int(4 * pow(4, n - 1));
 	return n;
 }
 
-void zoomViewAt(sf::Vector2i pixel, sf::RenderWindow &window, sf::View &view, float zoom)
+void zoomViewAt(sf::Vector2i pixel, sf::RenderWindow &window, sf::View &view, float factor)
 {
 	const sf::Vector2f beforeCoord{window.mapPixelToCoords(pixel)};
-	view.zoom(zoom);
+	view.zoom(factor);
 	window.setView(view);
 	const sf::Vector2f afterCoord{window.mapPixelToCoords(pixel)};
 	const sf::Vector2f offsetCoords{beforeCoord - afterCoord};
@@ -42,12 +38,11 @@ HilbertCurve(int n, sf::VertexArray &curve, double x0 = 0.0, double y0 = 0.0, do
 		double Y = y0 + (xj + yj) / 2.0;
 		// Output the coordinates of the curve
 		sf::Color color(
-			255 - 128 * curve.getVertexCount() / n_verts,
-			128 + 128 * curve.getVertexCount() / n_verts,
-			128 * curve.getVertexCount() / n_verts);
+			255 - 128 * curve.getVertexCount() / vertices,
+			128 + 128 * curve.getVertexCount() / vertices,
+			128 * curve.getVertexCount() / vertices);
 
 		curve.append(sf::Vertex(sf::Vector2f(SIZE * X, SIZE * Y), color));
-		updated = true;
 		return;
 	}
 	HilbertCurve(n - 1, curve, x0, y0, yi / 2.0, yj / 2.0, xi / 2.0, xj / 2.0);
@@ -58,6 +53,7 @@ HilbertCurve(int n, sf::VertexArray &curve, double x0 = 0.0, double y0 = 0.0, do
 	HilbertCurve(n - 1, curve, x0 + xi / 2.0 + yi, y0 + xj / 2.0 + yj, -yi / 2.0, -yj / 2.0, -xi / 2.0, -xj / 2.0);
 
 }
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(SIZE, SIZE), "Hilbert Curve");
@@ -67,12 +63,14 @@ int main()
 	window.setView(view);
 
 	int n = 1;
+	bool updated = true;
+	bool held = false;
+	float zoom = 1.0;
+
 	sf::Vector2i pos = sf::Mouse::getPosition(window);
 	sf::Vector2i old_pos = pos;
 
 	sf::VertexArray curve(sf::LineStrip);
-	sf::Transform transform;
-	transform.translate(0, 0);
 	HilbertCurve(n, curve);
 
 	while (window.isOpen()) {
@@ -84,15 +82,17 @@ int main()
 
 			} else if (event.type == sf::Event::Resized) {
 				// update the view to the new size of the window
-				view.setSize(event.size.width, event.size.height);
+				view.setSize(float(event.size.width), float(event.size.height));
 				window.setView(view);
 				updated = true;
 
 			} else if (event.type == sf::Event::KeyPressed) {
 				if (event.key.code == sf::Keyboard::Up) {
+					updated = true;
 					curve.clear();
 					HilbertCurve(increment(n), curve);
 				} else if (event.key.code == sf::Keyboard::Down) {
+					updated = true;
 					curve.clear();
 					HilbertCurve(decrement(n), curve);
 				}
@@ -103,10 +103,11 @@ int main()
 			} else if (event.type == sf::Event::MouseButtonReleased) {
 				held = false;
 			} else if (event.type == sf::Event::MouseMoved && held) {
-				pos = sf::Mouse::getPosition(window);
-				transform.translate(sf::Vector2f{pos - old_pos} * zoom);
-				old_pos = pos;
 				updated = true;
+				pos = sf::Mouse::getPosition(window);
+				view.move(sf::Vector2f{old_pos - pos} * zoom);
+				window.setView(view);
+				old_pos = pos;
 			} else if (event.type == sf::Event::MouseWheelScrolled) {
 				updated = true;
 				if (event.mouseWheelScroll.delta > 0) {
@@ -122,7 +123,7 @@ int main()
 		}
 		if (updated) {
 			window.clear(sf::Color::White);
-			window.draw(curve, transform);
+			window.draw(curve);
 			window.display();
 			updated = false;
 		}
