c60a41dcc0e095ce17283cabc08dba4f73868e35
eisendaniel <danieleisen99@gmail.com>
2020-05-10 12:40:29 +1200

    clion update

---
+++ HilbertCurve.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Daniel on 15/02/2020.
+//
+
---
+++ HilbertCurve.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Daniel on 15/02/2020.
+//
+
+#ifndef _HILBERTCURVE_HPP_
+#define _HILBERTCURVE_HPP_
+
+#endif //_HILBERTCURVE_HPP_
